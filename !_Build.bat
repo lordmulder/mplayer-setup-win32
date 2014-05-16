8675f3e7ce797afaf5d6015cffc14fbe12c8477d
LoRd_MuldeR <mulder2@gmx.de>
2014-05-16 14:01:50 +0200

    Updated MPlayer binaries to r37199, which is supposed to fix a DVD playback issue.

--- !_Build.bat
+++ !_Build.bat
@@ -1,8 +1,8 @@
 @echo off
 
 REM Build Number and other version info
-set "BUILD_NO=124"
-set "MPLAYER_REVISION=37182"
+set "BUILD_NO=125"
+set "MPLAYER_REVISION=37199"
 set "SMPLAYER_VERSION=14.3.0 (SVN-r6219)"
 set "MPUI_VERSION=1.2-pre3 (Build 38)"
 set "CODECS_DATE=2011-01-31"
