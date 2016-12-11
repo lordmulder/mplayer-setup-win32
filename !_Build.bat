681d7b82d2607e10489eb12aad589b7ad7eaf816
LoRd_MuldeR <mulder2@gmx.de>
2016-12-11 20:59:08 +0100

    Switch to 7Zip-based SFX wrapper.

--- !_Build.bat
+++ !_Build.bat
@@ -48,21 +48,29 @@ mkdir "%~dp0\.Release" 2> NUL
 
 REM Build update tool
 "%MAKE_NSIS%" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DUPX_PATH=%UPX_PATH%" "/DMPLAYER_OUTFILE=%~dp0\.Compile\Updater.exe" "%~dp0\MPUI_Updater.nsi"
-if errorlevel 1 (
+if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
 
 REM Build main installer
 "%MAKE_NSIS%" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DMPLAYER_REVISION=%MPLAYER_REVISION%" "/DSMPLAYER_VERSION=%SMPLAYER_VERSION%" "/DMPUI_VERSION=%MPUI_VERSION%" "/DCODECS_DATE=%CODECS_DATE%" "/DUPX_PATH=%UPX_PATH%" "/DMPLAYER_OUTFILE=%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\MPUI_Setup.nsi"
-if errorlevel 1 (
+if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
 
 REM Build installer wrapper
-"%MAKE_NSIS%" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DMPLAYER_REVISION=%MPLAYER_REVISION%" "/DUPX_PATH=%UPX_PATH%" "/DMPLAYER_SRCFILE=%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "/DMPLAYER_OUTFILE=%~dp0\.Release\MPUI.%ISO_DATE%.exe" "%~dp0\MPUI_Wrapper.nsi"
-if errorlevel 1 (
+call "%~dp0\Utils\7zSD.cmd" "%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "MPlayer for Windows" "MPUI-Setup-r%BUILD_NO%"
+if %ERRORLEVEL% NEQ 0 (
+	pause
+	goto:eof
+)
+
+set "VERPATCH_PRODUCT=MPlayer for Windows (Installer)"
+set "VERPATCH_FILEVER=%ISO_DATE:-=.%.%BUILD_NO%"
+"%~dp0\Utils\VerPatch.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "%VERPATCH_FILEVER%" /pv "%VERPATCH_FILEVER%" /fn /s desc "%VERPATCH_PRODUCT%" /s product "%VERPATCH_PRODUCT%" /s title "MPUI Setup SFX" /s copyright "Copyright (C) LoRd_MuldeR" /s company "Free Software Foundation"
+if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
