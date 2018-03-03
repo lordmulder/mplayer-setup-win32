e513e3e7eb134dcaae821deb0d04fc75e1a40877
LoRd_MuldeR <mulder2@gmx.de>
2018-03-03 13:24:36 +0100

    Removed some tools that are now used from "prerequisites" directory.

--- !_Build.bat
+++ !_Build.bat
@@ -7,11 +7,20 @@ set "SMPLAYER_VERSION=16.11.0 (SVN-r8243)"
 set "MPUI_VERSION=1.2-pre3 (Build 38)"
 set "CODECS_DATE=2011-01-31"
 
+REM Prerequisites base path
+set "PREREQUISITES_DIR=E:\Source\Prerequisites"
+
 REM Path to NSIS, Unicode version highly recommended!
-set "NSIS_PATH=E:\Source\Prerequisites\NSIS\makensis.exe"
+set "NSIS_PATH=%PREREQUISITES_DIR%\NSIS"
 
 REM Path to UPX executable compressor program
-set "UPX_PATH=E:\Source\Prerequisites\UPX\UPX.exe"
+set "UPX_PATH=%PREREQUISITES_DIR%\UPX"
+
+REM Path to 7-Zip tools
+set "SEVENZ_PATH=%PREREQUISITES_DIR%\SevenZip"
+
+REM Path to VerPatch tool
+set "VPATCH_PATH=%PREREQUISITES_DIR%\VerPatch"
 
 REM --------------------------------------------------------------------------
 REM Do NOT modify any lines below!
@@ -32,7 +41,7 @@ for /F "tokens=1,2 delims=:" %%a in ('"%~dp0\Utils\Date.exe" +ISODATE:%%Y-%%m-%%
 )
 
 REM Check for MakeNSIS
-if not exist "%MAKE_NSIS%\makensis.exe" (
+if not exist "%NSIS_PATH%\makensis.exe" (
 	echo MAKENSIS executable not found, check path!
 	pause
 	goto:eof
@@ -47,36 +56,36 @@ mkdir "%~dp0\.Compile" 2> NUL
 mkdir "%~dp0\.Release" 2> NUL
 
 REM Build update tool
-"%MAKE_NSIS%\makensis.exe" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DUPX_PATH=%UPX_PATH%" "/DMPLAYER_OUTFILE=%~dp0\.Compile\Updater.exe" "%~dp0\MPUI_Updater.nsi"
+"%NSIS_PATH%\makensis.exe" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DUPX_PATH=%UPX_PATH%\UPX.exe" "/DMPLAYER_OUTFILE=%~dp0\.Compile\Updater.exe" "%~dp0\MPUI_Updater.nsi"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
-"%MAKE_NSIS%\peheader.exe" "%~dp0\.Compile\Updater.exe"
+"%NSIS_PATH%\peheader.exe" "%~dp0\.Compile\Updater.exe"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
 
 REM Build main installer
-"%MAKE_NSIS%\makensis.exe" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DMPLAYER_REVISION=%MPLAYER_REVISION%" "/DSMPLAYER_VERSION=%SMPLAYER_VERSION%" "/DMPUI_VERSION=%MPUI_VERSION%" "/DCODECS_DATE=%CODECS_DATE%" "/DUPX_PATH=%UPX_PATH%" "/DMPLAYER_OUTFILE=%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\MPUI_Setup.nsi"
+"%NSIS_PATH%\makensis.exe" "/DMPLAYER_BUILDNO=%BUILD_NO%" "/DMPLAYER_DATE=%ISO_DATE%" "/DMPLAYER_REVISION=%MPLAYER_REVISION%" "/DSMPLAYER_VERSION=%SMPLAYER_VERSION%" "/DMPUI_VERSION=%MPUI_VERSION%" "/DCODECS_DATE=%CODECS_DATE%" "/DUPX_PATH=%UPX_PATH%\UPX.exe" "/DMPLAYER_OUTFILE=%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\MPUI_Setup.nsi"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
-"%MAKE_NSIS%\peheader.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.sfx"
+"%NSIS_PATH%\peheader.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.sfx"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
 
 REM Build installer wrapper
-call "%~dp0\Utils\7zSD.cmd" "%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "MPlayer for Windows" "MPUI-Setup-r%BUILD_NO%"
+call "%SEVENZ_PATH%\7zSD.cmd" "%~dp0\.Release\MPUI.%ISO_DATE%.sfx" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "MPlayer for Windows" "MPUI-Setup-r%BUILD_NO%"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
 )
-"%MAKE_NSIS%\peheader.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.exe"
+"%NSIS_PATH%\peheader.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.exe"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
@@ -84,7 +93,7 @@ if %ERRORLEVEL% NEQ 0 (
 
 set "VERPATCH_PRODUCT=MPlayer for Windows (Installer)"
 set "VERPATCH_FILEVER=%ISO_DATE:-=.%.%BUILD_NO%"
-"%~dp0\Utils\VerPatch.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "%VERPATCH_FILEVER%" /pv "%VERPATCH_FILEVER%" /fn /s desc "%VERPATCH_PRODUCT%" /s product "%VERPATCH_PRODUCT%" /s title "MPUI Setup SFX" /s copyright "Copyright (C) LoRd_MuldeR" /s company "Free Software Foundation"
+"%VPATCH_PATH%\VerPatch.exe" "%~dp0\.Release\MPUI.%ISO_DATE%.exe" "%VERPATCH_FILEVER%" /pv "%VERPATCH_FILEVER%" /fn /s desc "%VERPATCH_PRODUCT%" /s product "%VERPATCH_PRODUCT%" /s title "MPUI Setup SFX" /s copyright "Copyright (C) LoRd_MuldeR" /s company "Free Software Foundation"
 if %ERRORLEVEL% NEQ 0 (
 	pause
 	goto:eof
